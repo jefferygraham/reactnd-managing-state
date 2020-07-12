a9f28bf60f518285b616d15fb0d0b361a43cb149
Jeffery L. Graham <jeffery.lemar.graham@gmail.com>
2020-07-12 20:04:18 +0000

    add files

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  	state = {
+      	value1: Math.floor(Math.random() * 100),
+		value2: Math.floor(Math.random() * 100),
+		value3: Math.floor(Math.random() * 100),
+		proposedAnswer: Math.floor(Math.random() * 3) + Math.floor(Math.random() * 100) * 3,
+		numQuestions: 0,
+		numCorrect: 0
+    }
+  	
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="game">
+          <h2>Mental Math</h2>
+          <div className="equation">
+            <p className="text">{`${this.state.value1} + ${this.state.value2} + ${this.state.value3} = ${this.state.proposedAnswer}`}</p>
+          </div>
+          <button>True</button>
+          <button>False</button>
+          <p className="text">
+            Your Score: {this.state.numCorrect}/{this.state.numQuestions}
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
